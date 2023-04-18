8a81acbcd19537d98dfd3a23d78786a1f1dcdee3
Dima Bolushev <bdima3003@mail.ru>
2023-04-18 22:53:25 +0300

    okLessonTwoCommitTwoTest

--- src/test/java/org/example/LessonSixOne/ru/netology/services/monthServiceTest.java
+++ src/test/java/org/example/LessonSixOne/ru/netology/services/monthServiceTest.java
@@ -2,8 +2,23 @@ package org.example.LessonSixOne.ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class monthServiceTest {
+
+    // Задание 2
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/budget.csv")
+    public void test(int expected, int income, int expenses, int threshold) {
+        monthService service = new monthService();
+
+        //int expected = 2;
+        int actual = service.calculate(income, expenses, threshold);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Задание 1
     @Test
     public void test() {
         monthService service = new monthService();
